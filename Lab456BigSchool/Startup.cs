7a2a8bb0f9ed9133ed87cefa9fc90a0c3b23c137
Thanh Chinh <Thanh Chinh@CHINHPC>
2021-07-06 21:35:14 +0700

    Add project files.

---
+++ Lab456BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab456BigSchool.Startup))]
+namespace Lab456BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Lab456BigSchool/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Lab456BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab456BigSchool.ViewModels
+{
+    public class CoursesViewModel
+
+    {
+        public IEnumerable<Course> UpcommingCourses { get; set; }
+        public bool ShowAction { get; set; }
+    }
+}
